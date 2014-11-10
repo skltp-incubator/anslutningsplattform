9f0e716f0a913fda0784fa839e94ab2f36120547
Jonas Behmer <jonas.behmer@callistaenterprise.se>
2014-11-10 14:39:28 +0100

    AV-12 Added bootstrap and angular dependencies

--- anslutningsverktyg/grails-app/conf/ApplicationResources.groovy
+++ anslutningsverktyg/grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,18 @@ modules = {
     jquery {
         resource url: 'js/lib/jquery/dist/jquery.js'
     }
+
+    bootstrap {
+        dependsOn: 'jquery'
+        resource url: 'js/lib/bootstrap/dist/css/bootstrap.css', disposition: 'head'
+    }
+
+    angular {
+        resource url: 'js/lib/angular/angular.js', disposition: 'head'
+        resource url: 'js/lib/angular-cookies/angular-cookies.js', disposition: 'head'
+        resource url: 'js/lib/angular-resource/angular-resource.js', disposition: 'head'
+        resource url: 'js/lib/angular-route/angular-route.js', disposition: 'head'
+        resource url: 'js/lib/angular-sanitize/angular-sanitize.js', disposition: 'head'
+        resource url: 'js/lib/angular-animate/angular-animate.js', disposition: 'head'
+    }
 }
