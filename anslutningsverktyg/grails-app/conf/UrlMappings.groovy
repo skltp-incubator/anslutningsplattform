55ba9cb0cd496dcf04606e446b2436fb53b53fee
Jonas Behmer <jonas.behmer@callistaenterprise.se>
2014-11-10 14:08:28 +0100

    AV-12 Making sure we can serve static files from the web-app folder with relevant url-mappings

--- anslutningsverktyg/grails-app/conf/UrlMappings.groovy
+++ anslutningsverktyg/grails-app/conf/UrlMappings.groovy
@@ -1,21 +1,26 @@
 class UrlMappings {
 
-	static mappings = {
-		
-		//API
-		"/api/tjanstekomponenter"(version:'1.0', namespace:'v1', controller: 'tjansteKomponentApi')
-		"/api/producentbestallningar"(version:'1.0', namespace:'v1', controller: 'producentBestallningApi')
-		"/api/hsa"(version:'1.0', namespace:'v1', controller: 'hsaApi')
-		
-		//Controllers
-        "/$controller/$action?/$id?(.$format)?"{
+    static mappings = {
+
+        //Controllers
+        "/$controller/$action?/$id?(.$format)?" {
             constraints {
                 // apply constraints here
             }
         }
-		
+        //API
+        group "/api", {
+            "/tjanstekomponenter"(version: '1.0', namespace: 'v1', controller: 'tjansteKomponentApi')
+            "/producentbestallningar"(version: '1.0', namespace: 'v1', controller: 'producentBestallningApi')
+            "/hsa"(version: '1.0', namespace: 'v1', controller: 'hsaApi')
+        }
 
-        "/"(view:"/index")
-        "500"(view:'/error')
-	}
+
+        //This is the way to serve static resource from the web-app folders
+        "/anslut"(uri: "views/tjansteproducent/anslut.html")
+
+        "/"(view: "/index")
+        "500"(view: '/error')
+    }
+
 }
