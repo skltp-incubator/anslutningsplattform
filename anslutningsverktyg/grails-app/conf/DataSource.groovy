4f8aa2e823fe2c57c2369452ec0237b2327b2f13
Jonas Behmer <jonas.behmer@callistaenterprise.se>
2014-11-21 08:15:28 +0100

    AV-37 Datasource for prod

--- anslutningsverktyg/grails-app/conf/DataSource.groovy
+++ anslutningsverktyg/grails-app/conf/DataSource.groovy
@@ -30,8 +30,7 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "create-drop"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            jndiName = "java:comp/env/jdbc/AP"
         }
     }
 }
