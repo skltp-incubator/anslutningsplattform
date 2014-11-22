c479849f5036812bf399cec5312da84f7b3a2142
Hans Thunberg <hans.thunberg@callistaenterprise.se>
2014-11-22 19:03:26 +0100

    Only bootstrap testdata when in environment development

--- anslutningsverktyg/grails-app/conf/BootStrap.groovy
+++ anslutningsverktyg/grails-app/conf/BootStrap.groovy
@@ -5,77 +5,86 @@ import org.apache.shiro.crypto.hash.Sha256Hash
 
 class BootStrap {
 
+	def grailsApplication
+
 	def init = { servletContext ->
 
-		//Roles
-		def adminRole = new Role(name: 'ADMINISTRATÖR')
-		adminRole.addToPermissions("BestallningsHistorik:index")
-		adminRole.addToPermissions("LogiskAdress:index")
-		adminRole.addToPermissions("Role:*")
-		adminRole.addToPermissions("User:*")
-		adminRole.addToPermissions("Tjanstekomponent:index")
-		adminRole.save(failOnError:true)
-		
-		def tkAnsvarigRole = new Role(name: 'TJÄNSTEKOMPONENTANSVARIG')
-		tkAnsvarigRole.addToPermissions("BestallningsHistorik:*")
-		tkAnsvarigRole.addToPermissions("LogiskAdress:*")
-		tkAnsvarigRole.addToPermissions("Tjanstekomponent:*")
-		tkAnsvarigRole.addToPermissions("ProdcentAnslutning:*")
-		tkAnsvarigRole.addToPermissions("KonsumentAnslutning:*")
-		tkAnsvarigRole.save(failOnError:true)
+		environments {
+			production {
+
+			}
+			development {
+				//Roles
+				def adminRole = new Role(name: 'ADMINISTRATÖR')
+				adminRole.addToPermissions("BestallningsHistorik:index")
+				adminRole.addToPermissions("LogiskAdress:index")
+				adminRole.addToPermissions("Role:*")
+				adminRole.addToPermissions("User:*")
+				adminRole.addToPermissions("Tjanstekomponent:index")
+				adminRole.save(failOnError:true)
+
+				def tkAnsvarigRole = new Role(name: 'TJÄNSTEKOMPONENTANSVARIG')
+				tkAnsvarigRole.addToPermissions("BestallningsHistorik:*")
+				tkAnsvarigRole.addToPermissions("LogiskAdress:*")
+				tkAnsvarigRole.addToPermissions("Tjanstekomponent:*")
+				tkAnsvarigRole.addToPermissions("ProdcentAnslutning:*")
+				tkAnsvarigRole.addToPermissions("KonsumentAnslutning:*")
+				tkAnsvarigRole.save(failOnError:true)
 
 
-		//Admin user
-		def adminUser = new User(
-				namn: "Agda Andersson",
-				username: "admin",
-				epost: "admin@lorumipsum.nu",
-				datumSkapad: new Date(),
-				passwordHash: new Sha256Hash("password").toHex())
-		
-		adminUser.addToRoles(adminRole)
-		adminUser.save(failOnError:true)
-		
-		//Tjanstekomponent responsible user
-		def tkAnsvarig = new User(
-			namn: "Jöns Jönsson",
-			username: "user",
-			epost: "user@lorumipsum.nu",
-			datumSkapad: new Date(),
-			passwordHash: new Sha256Hash("password").toHex())
-	
-		tkAnsvarig.addToRoles(tkAnsvarigRole)
-		tkAnsvarig.save(failOnError:true)
-		
-		def tk1 = new TjansteKomponent(
-			hsaId: "HSASERVICES-123Q",
-			tekniskKontaktEpost: "kontakten@lorumipsum.nu",
-			tekniskKontaktNamn: "Tolvan Tolvansson",
-			tekniskKontaktTelefon: "1234567890",
-			funktionsBrevladaEpost: "funktionsbrevladan@lorumipsum.nu",
-			funktionsBrevladaTelefon: "0987654321",
-			ipadress: "",
-			user: tkAnsvarig).save(failOnError:true)
-			
-		def tk2 = new TjansteKomponent(
-			hsaId: "HSASERVICES-123	Z",
-			tekniskKontaktEpost: "kontakten@lorumipsum.nu",
-			tekniskKontaktNamn: "Tolvan Tolvansson",
-			tekniskKontaktTelefon: "1234567890",
-			funktionsBrevladaEpost: "funktionsbrevladan@lorumipsum.nu",
-			funktionsBrevladaTelefon: "0987654321",
-			ipadress: "",
-			user: tkAnsvarig).save(failOnError:true)
-		
-		new ProducentBestallning(
-			miljo: "TEST", 
-			status: BestallningsStatus.NY,
-			tjansteKomponent: tk1).save(failOnError:true)
-			
-		new ProducentBestallning(
-			miljo: "SIT",
-			status: BestallningsStatus.NY,
-			tjansteKomponent: tk1).save(failOnError:true)
+				//Admin user
+				def adminUser = new User(
+						namn: "Agda Andersson",
+						username: "admin",
+						epost: "admin@lorumipsum.nu",
+						datumSkapad: new Date(),
+						passwordHash: new Sha256Hash("password").toHex())
+
+				adminUser.addToRoles(adminRole)
+				adminUser.save(failOnError:true)
+
+				//Tjanstekomponent responsible user
+				def tkAnsvarig = new User(
+						namn: "Jöns Jönsson",
+						username: "user",
+						epost: "user@lorumipsum.nu",
+						datumSkapad: new Date(),
+						passwordHash: new Sha256Hash("password").toHex())
+
+				tkAnsvarig.addToRoles(tkAnsvarigRole)
+				tkAnsvarig.save(failOnError:true)
+
+				def tk1 = new TjansteKomponent(
+						hsaId: "HSASERVICES-123Q",
+						tekniskKontaktEpost: "kontakten@lorumipsum.nu",
+						tekniskKontaktNamn: "Tolvan Tolvansson",
+						tekniskKontaktTelefon: "1234567890",
+						funktionsBrevladaEpost: "funktionsbrevladan@lorumipsum.nu",
+						funktionsBrevladaTelefon: "0987654321",
+						ipadress: "",
+						user: tkAnsvarig).save(failOnError:true)
+
+				def tk2 = new TjansteKomponent(
+						hsaId: "HSASERVICES-123	Z",
+						tekniskKontaktEpost: "kontakten@lorumipsum.nu",
+						tekniskKontaktNamn: "Tolvan Tolvansson",
+						tekniskKontaktTelefon: "1234567890",
+						funktionsBrevladaEpost: "funktionsbrevladan@lorumipsum.nu",
+						funktionsBrevladaTelefon: "0987654321",
+						ipadress: "",
+						user: tkAnsvarig).save(failOnError:true)
+
+				new ProducentBestallning(
+						miljo: "TEST",
+						status: BestallningsStatus.NY,
+						tjansteKomponent: tk1).save(failOnError:true)
+
+				new ProducentBestallning(
+						miljo: "SIT",
+						status: BestallningsStatus.NY,
+						tjansteKomponent: tk1).save(failOnError:true)
+			}
+		}
 	}
 	
 	def destroy = {
---
+++ resources/testdata.sql
@@ -0,0 +1,36 @@
+--
+-- Dumpning av testdata
+--
+
+INSERT INTO `role` (`id`, `version`, `name`) VALUES
+(3, 0, 'ADMINISTRATÖR'),
+(4, 0, 'TJÄNSTEKOMPONENTANSVARIG');
+
+INSERT INTO `role_permissions` (`role_id`, `permissions_string`) VALUES
+(3, 'User:*'),
+(3, 'Role:*'),
+(3, 'Tjanstekomponent:index'),
+(3, 'BestallningsHistorik:index'),
+(3, 'LogiskAdress:index'),
+(4, 'ProdcentAnslutning:*'),
+(4, 'KonsumentAnslutning:*'),
+(4, 'LogiskAdress:*'),
+(4, 'Tjanstekomponent:*'),
+(4, 'BestallningsHistorik:*');
+
+INSERT INTO `user` (`id`, `version`, `datum_skapad`, `datum_uppdaterad`, `epost`, `namn`, `password_hash`, `telefon_nummer`, `username`) VALUES
+(3, 0, '2014-11-21 14:47:55', NULL, 'admin@lorumipsum.nu', 'Agda Andersson', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', NULL, 'admin'),
+(4, 0, '2014-11-21 14:47:55', NULL, 'user@lorumipsum.nu', 'Jöns Jönsson', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', NULL, 'user');
+
+
+INSERT INTO `user_roles` (`role_id`, `user_id`) VALUES
+(3, 3),
+(4, 4);
+
+INSERT INTO `tjanste_komponent` (`id`, `version`, `funktions_brevlada_epost`, `funktions_brevlada_telefon`, `hsa_id`, `ipadress`, `teknisk_kontakt_epost`, `teknisk_kontakt_namn`, `teknisk_kontakt_telefon`, `user_id`) VALUES
+(3, 0, 'funktionsbrevladan@lorumipsum.nu', '0987654321', 'HSASERVICES-123Q', NULL, 'kontakten@lorumipsum.nu', 'Tolvan Tolvansson', '1234567890', 4),
+(4, 0, 'funktionsbrevladan@lorumipsum.nu', '0987654321', 'HSASERVICES-123	Z', NULL, 'kontakten@lorumipsum.nu', 'Tolvan Tolvansson', '1234567890', 4);
+
+INSERT INTO `producent_bestallning` (`id`, `version`, `miljo`, `status`, `tjanste_komponent_id`) VALUES
+(3, 0, 'TEST', 'NY', 3),
+(4, 0, 'SIT', 'NY', 3);
