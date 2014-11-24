cb8863e007b2f548f3e8a131de5ee7ba655086ce
Håkan Dahl <hakan@Hakans-MacBook-Pro.local>
2014-11-24 12:15:59 +0100

    AV-56 Lagt till setenv.sh för Tomcat, sätter sysproperty som pekar ut conf-dir.

---
+++ resources/tomcat/setenv.sh
@@ -0,0 +1,3 @@
+#export CATALINA_OPTS="$CATALINA_OPTS -Xms256m -Xmx512m -XX:MaxPermSize=256m"
+
+export CATALINA_OPTS="$CATALINA_OPTS -Dse.skltp.av.config.dir=/usr/local/skltp-anslutningsplattform/conf"
