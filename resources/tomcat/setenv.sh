676ef7e3b1327c50db7fa5ccea5bde1c987698b5
Hans Thunberg <hans.thunberg@callistaenterprise.se>
2014-11-25 16:54:23 +0100

    AV-56: Uppdaterade setenv med korrekt namn i variabel ap istället för av

--- resources/tomcat/setenv.sh
+++ resources/tomcat/setenv.sh
@@ -1,3 +1,3 @@
 #export CATALINA_OPTS="$CATALINA_OPTS -Xms256m -Xmx512m -XX:MaxPermSize=256m"
 
-export CATALINA_OPTS="$CATALINA_OPTS -Dse.skltp.av.config.dir=/usr/local/skltp-anslutningsplattform/conf"
+export CATALINA_OPTS="$CATALINA_OPTS -Dse.skltp.ap.config.dir=/usr/local/skltp-anslutningsplattform/conf"
